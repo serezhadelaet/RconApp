86fdf3dbbb9e90708c7b2b2e3ab29b2973d6b0d5
serezhadelaet <fr3sh00@gmail.com>
2020-02-09 04:39:48 +0400

    newserveractivity cleanup

--- app/src/main/java/com/example/rconapp/NewServerActivity.java
+++ app/src/main/java/com/example/rconapp/NewServerActivity.java
@@ -4,15 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class NewServerActivity extends Activity {
 
